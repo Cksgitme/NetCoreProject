e06b37f209797ad7df166b089e808b4f457af5f8
Cksgitme <cksrajsingh@gmail.com>
2020-06-05 14:00:22 +0530

    added

---
+++ NETCoreTestLibrary/Interface/ITestLibrary.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NETCoreTestLibrary
+{
+   public interface ITestLibrary
+    {
+         void LoadAppSettings(string name);
+    }
+}
---
+++ NETCoreTestLibrary/Uow/TestLibrary.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Configuration;
+
+namespace NETCoreTestLibrary
+{
+    public class TestLibrary:ITestLibrary
+    {
+        public void LoadAppSettings(string name)
+        {
+            string serachPattern =
+            ConfigurationManager.AppSettings["Name"];
+        }
+    }
+}
